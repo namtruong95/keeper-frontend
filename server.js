2baed3055c34a00cf019a0d0e77e2e0352f4cadd
Truong Thanh Nam <nam.vboys@gmail.com>
2017-08-25 20:19:58 +0700

    conf to deploy example

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+const port = process.env.PORT || '3001';
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => { console.log(`Server started, use port ${port}`) });
