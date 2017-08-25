878ef14823210972c8554cdd916ef19fcd0c9644
Truong Thanh Nam <nam.vboys@gmail.com>
2017-08-25 20:06:30 +0700

    Initialize the project

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const gulp = require('gulp');
+const gzip = require('gulp-gzip');
+
+gulp.task('compress', () => {
+  gulp.src(['./dist/*.js', './dist/*.css'])
+    .pipe(gzip())
+    .pipe(gulp.dest('./dist'));
+});
